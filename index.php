4f5fbc8092d4c7e180c95bc1e39c5c71ed3d6f6b
Kirill Bystrov <kir9aza@gmail.com>
2022-11-11 23:39:24 +0300

    unique numbers ver. 2

--- index.php
+++ index.php
@@ -6,7 +6,21 @@
  -->
 <?php
 $arrNumbers = [412,41,21,543,654,12,654,12,21];
+$newArr = [];
+//-------------Способ 1------------------------
 foreach (array_unique($arrNumbers) as $value) {
+	echo "{$value} ";
+}
+echo "<br> <br>";
+
+//-------------Способ 2-------------------------
+for ($i = 0; $i < count($arrNumbers); $i++) {
+	if (!isset($newArr[$arrNumbers[$i]])) {
+		$newArr[$arrNumbers[$i]] = $arrNumbers[$i];  
+	}
+}
+
+foreach ($newArr as $value) {
 	echo "{$value} ";
 }
 ?>
